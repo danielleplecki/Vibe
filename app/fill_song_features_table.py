fc3ff45acae81588b4bdc319b91236c0f85081f3
Dickson <cjdicks2@sp19-cs411-52.cs.illinois.edu>
2019-04-07 22:38:34 -0500

    finished fill song features

--- app/fill_song_features_table.py
+++ app/fill_song_features_table.py
@@ -6,21 +6,31 @@ cursor = conn.cursor()
 def fill_song_features_table():
     
     sql = """INSERT INTO songs (
-    duration_ms, preview_url, track_key, modality, time_signature,
+    duration_ms, track_key, modality, time_signature,
     acousticness, danceability, energy, instrumentalness,
     liveness, loudness, speechiness, valence, tempo, ID, uri
     ) 
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
 
-    for track in tracks:
-	duration_ms = track[
+    duration_ms = features['duration_ms']
+    track_key = features['key']  
+    modality = features['mode'] 
+    time_signature = features['time_signature'] 
+    acousticness = features['acousticness'] 
+    danceability = features['danceability'] 
+    energy = features['energy'] 
+    instrumentalness = features['instrumentalness'] 
+    liveness = features['liveness'] 
+    loudness = features['loudness'] 
+    speechiness = features['speechiness'] 
+    valence = features['valence'] 
+    tempo = features['tempo'] 
+    ID = features['id']
+    uri = features['uri'] 
 
-
-	name = track['name']
-	artist = track['artists']['name']
-	album_name = track['album']['name']	
-
-    val = (ID, name, artist, album_name)
+    val = (duration_ms, track_key, modality, time_signature, acousticness,
+danceability, energy, instrumentalness, liveness, loudness, speechiness,
+valence, tempo, ID, uri)
     cursor.execute(sql, val)
 
     
