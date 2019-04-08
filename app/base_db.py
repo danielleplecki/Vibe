067866999f60ab5e0260350571414223d39c0e64
aebrown22 <aebrown22@me.com>
2019-04-08 02:20:47 -0500

    added base_db file

---
+++ app/base_db.py
@@ -0,0 +1,20 @@
+import mysql.connector as db
+conn = db.connect(user='root', password='', database='vibe')
+cursor = conn.cursor()
+
+def query(stmt, vals=()):
+    cursor.execute(stmt, vals)
+    results = []
+    for row in cursor.fetchall():
+        results.append({cursor.description[i][0] : value for i, value in enumerate(row)})
+    return results
+
+def delete(stmt, vals=()):
+    cursor.execute(stmt, vals)
+    conn.commit()
+    return cursor.rowcount
+
+def insert(stmt, vals=()):
+    cursor.execute(stmt, vals)
+    conn.commit()
+    return cursor.rowcount
